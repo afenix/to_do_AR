60afb2dbf86aab6e1fd295ece91ad1a701c938ec
James Maynard & Alister A. Fenix <student@epicodus.com>
2015-01-26 12:27:17 -0800

    add active record association methods to list and task class with passing specs.

--- lib/task.rb
+++ lib/task.rb
@@ -3,4 +3,6 @@ class Task < ActiveRecord::Base
   scope(:not_done, -> do
     where({:done => false})
   end)
+
+  belongs_to(:list)
 end
--- spec/task_spec.rb
+++ spec/task_spec.rb
@@ -2,6 +2,12 @@ require("spec_buddy")
 
 describe(Task) do
 
+  it("tells which list it belongs to") do
+    list = List.create({ :name => "My List" })
+    task = Task.create({ :description => "Rule the world", :list_id => list.id })
+    expect(task.list()).to eq(list)
+  end
+
   describe(".not_done") do
     it("will return only tasks that are not yet completed") do
     test_task1 = Task.create({:description => "Rule the world", :done => false})
